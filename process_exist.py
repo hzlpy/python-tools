1a2b3eef6bb20629bf0ee92cb2229761bce7dd54
Happy.Zhang <ihzlpy@gmail.com>
2016-09-27 15:14:53 +0800

    modify

--- process_exist.py
+++ process_exist.py
@@ -1,24 +1,18 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-mysql_process = '/root/phxsql/tools/mysqld.lock'
-phxbinlogsvr_process = '/root/phxsql/tools/phxbinlogsvr.lock'
-phxsqlproxy_process = '/root/phxsql/tools/phxsqlproxy.lock'
-os.system('ps -ef | grep mysqld | grep -v grep >%s' % mysql_process)
-os.system('ps -ef | grep phxbinlogsvr | grep -v grep >%s' % phxbinlogsvr_process)
-os.system('ps -ef | grep phxsqlproxy | grep -v grep >%s' % phxsqlproxy_process)
-if not(os.path.getsize(mysql_process)):
-    print('mysqld is not exist. Will you want to restart it ?')
-else:
-    print('mysqld is running.')
 
-if not(os.path.getsize(phxbinlogsvr_process)):
-    print('phxbinlogsvr is not exist. Will you want to restart it ?')
-else:
-    print('phxbinlogsvr is running.')
+def process_exist(process_name):
+    process_lock = '/root/phxsql/tools/' + process_name + '.lock'
+    # print(process_lock)
+    os.system('ps -ef | grep %s | grep -v grep >%s' % (process_name, process_lock))
+    if not(os.path.getsize(process_lock)):
+	    print('%s is not exist. Will you want to restart it ?' % process_name)
+    else:
+	    print ('%s is running.' % process_name)
 
-if not(os.path.getsize(phxsqlproxy_process)):
-    print('phxsqlproxy is not exist. Will you want ti restart it ?')
-else:
-    print('phxsqlproxy is running.')
+if __name__ == '__main__':
+    process_exist('mysqld')
+    process_exist('phxbinlogsvr')
+    process_exist('phxsqlproxy')
 
